2ea1bd5f95bd92d736b0b9f5eb3484bce4302a9e
Gularte <47986817+thaizagularte@users.noreply.github.com>
2023-09-20 20:23:04 -0300

    Update interface

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import CharacterForm from './/addCharacters';
-import DeleteCharacterForm from './/delCharacter';
+import CharacterForm from './actions/addCharacters';
+import DeleteCharacter from './actions/delCharacter';
 
 function App() {
   const baseUrl = 'http://localhost:3001/characters';
   const [jsonData, setJsonData] = useState('');
   const [loading, setLoading] = useState(true); 
   const [isFormVisible, setIsFormVisible] = useState(false);
-  const [isDeleteFormVisible, setIsDeleteFormVisible] = useState(false);
-  
+ 
+
   useEffect(() => {
     axios.get(baseUrl)
       .then(response => {
-        setJsonData(response.data);
+        setJsonData(response.data.displayCharacters);
         setLoading(false); 
       })
       .catch(error => {
@@ -22,22 +22,69 @@ function App() {
       });
   }, []);
 
-  const handleDeleteClick = () => {
-    setIsDeleteFormVisible(true);
+  const handleDeleteClick = (characterId) => {
+    DeleteCharacter(characterId);
+  };
+
+  const handleEdit = (characterId) => {
+
   };
   
-
   return (
     <div>
-        <button onClick={() => setIsFormVisible(true)}>To add</button>
-        <button onClick={handleDeleteClick}>Delete</button>
-        {isFormVisible && <CharacterForm />}
-        {isDeleteFormVisible && <DeleteCharacterForm />} 
-        {loading ? (
-        <p>Carregando...</p>) : (
-            <pre>{JSON.stringify(jsonData, null, 2)}</pre>)}
-        </div>
-      );
+      <button onClick={() => setIsFormVisible(true)}> Adicionar </button>
+      { isFormVisible && <CharacterForm /> }
+      
+
+      {loading ? ( 
+        <p> Caregando ... </p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th> Name </th>
+              <th> Status </th>
+              <th> Species </th>
+              <th> Type </th>
+              <th> Gender </th>
+              <th> Created </th>
+              <th> Origin </th>
+              <th> Location </th>
+              <th> Image </th>
+              <th> Url </th>
+              <th> Actions </th>
+            </tr>
+          </thead>
+          <tbody>
+            {jsonData.map(character => (
+              <tr key={character.id}>
+                <td>{character.name}</td>
+                <td>{character.status}</td>
+                <td>{character.species}</td>
+                <td>{character.type}</td>
+                <td>{character.gender}</td>
+                <td>{character.created}</td>
+                <td>{character.originName}</td>
+                <td>{character.locationName}</td>
+                <td>
+                  <img src={character.image} alt={`Imagem de ${character.name}`} height={50} width={50} />
+                </td>
+                <td>{character.url}</td>
+                <td>
+                    <button>
+                      Edit
+                    </button>
+                    <button onClick={() => handleDeleteClick(character.id)}>
+                      Delete
+                    </button>
+                </td>       
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
     }
-    
-    export default App;
+    </div>
+)}
+
+export default App;
